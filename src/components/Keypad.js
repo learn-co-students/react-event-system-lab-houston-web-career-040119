841ea3fcc3bfb4aa84c8fbb934d63a16ac316699
Ubuntu <ubuntu@ip-172-31-89-97.ec2.internal>
2019-06-04 20:07:34 +0000

    done?

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,24 @@
 // Code Keypad Component Here
+import Reach from "react"
+
+class Keypad extends Reach.Component{
+    
+    keyUp=()=>{
+        console.log('Entering password...')
+    }
+    
+    
+    render(){
+        
+        return(
+            <div>
+                <input type="password" onKeyUp={this.keyUp}/>
+            </div>
+            
+            );
+
+    }
+
+}
+
+export default Keypad;
