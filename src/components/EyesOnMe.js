b30bc7496f957b2364f8fd0ea69357525f3b56d8
PYN <yxp7531540@gmail.com>
2019-06-03 22:16:59 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+class EyesOnMe extends Component {
+
+    render() {
+        return(
+            <button onFocus={this.goodEvent} onBlur={this.blurEvent}>what a button</button>
+        )
+    }
+    goodEvent = () => {
+        console.log('Good!')
+    } 
+
+    blurEvent = () => {
+        console.log('Hey! Eyes on me!')
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, {Component} from 'react';
+
+class Keypad extends Component {
+
+    render() {
+        return(
+            <input type="password" onKeyUp={this.event}/>
+        )
+    }
+
+    event = () => {
+        console.log('Entering password...')
+    }
+}
+
+export default Keypad
